79290d90e641e94f278184be736b6a3c31cf1aef
QEDady <amer.esmail48@gmail.com>
2020-11-10 23:59:38 +0200

    sounds second time

--- sounds.h
+++ sounds.h
@@ -1,13 +1,29 @@
 #ifndef SOUNDS_H
 #define SOUNDS_H
-
+#include <QMediaPlayer>
+#include<QMediaPlaylist>
 
 class Sounds
 {
+
+private:
+      QMediaPlaylist * Playlist;
+//sugessoion: make only one QMediaPlayer called Player
+//you wuill use to play whatever in PlayList
+//you can make one stop funciton
+      QMediaPlayer* backgroundmusic;
+      QMediaPlayer* winmusic;
+      QMediaPlayer* losemusic;
 public:
     Sounds();
-    void win();
-    void lose();
+    void PlayGeneral();
+    void StopGeneral();
+
+    void PlayWin();
+    void StopWin();
+
+    void PlayLose();
+    void StopLose();
 };
 
 #endif // SOUNDS_H
